45436be5646dff43c493302208a28dc44a4da76a
Ricky Dam <RickyDamOttawa@gmail.com>
2017-09-15 03:00:35 -0400

    Finished frontend of the 'Sell' tab

--- screens/sell.js
+++ screens/sell.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, TextInput, Picker, TouchableOpacity} from 'react-native';
+
 const styles = require('../styles.js');
 
 class SellScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textbookType: '',
+      textbookCondition: '',
+    }
+  }
   static navigationOptions = {
     tabBarLabel: 'Sell',
     tabBarIcon: ({tintColor}) => (
@@ -16,10 +24,73 @@ class SellScreen extends React.Component {
     const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text>This is the sell screen.</Text>
+
+        <Text style={styles.title}>Sell a textbook!</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Title"
+          underlineColorAndroid="transparent"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Author"
+          underlineColorAndroid="transparent"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Edition"
+          underlineColorAndroid="transparent"
+        />
+
+        <View style={styles.textbookCondition}>
+          <Picker
+            style={styles.textbookConditionPicker}
+            itemTextStyle={styles.testPickerItem}
+            selectedValue={this.state.textbookCondition}
+            onValueChange={(itemValue, itemIndex) => this.setState({textbookCondition: itemValue})}>
+            <Picker.Item label="Condition" value="Condition" />
+            <Picker.Item label="Like New" value="Like New" />
+            <Picker.Item label="Good" value="Good" />
+            <Picker.Item label="Okay" value="Okay" />
+          </Picker>
+        </View>
+
+        <View style={styles.textbookType}>
+          <Picker
+            style={styles.textbookTypePicker}
+            itemStyle={styles.testPickerItem}
+            selectedValue={this.state.textbookType}
+            onValueChange={(itemValue, itemIndex) => this.setState({textbookType: itemValue})}>
+            <Picker.Item label="Type" value="Type" />
+            <Picker.Item label="Paperback" value="Paperback" />
+            <Picker.Item label="Hardcover" value="Hardcover" />
+            <Picker.Item label="Looseleaf" value="Looseleaf" />
+          </Picker>
+        </View>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Course Code"
+          underlineColorAndroid="transparent"
+        />
+
+        <View style={styles.submitContainer}>
+          <TouchableOpacity onPress={theFunction}>
+            <View style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>SUBMIT</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
+}
+
+function theFunction() {
+  alert("Textbook listing has been successfully posted!");
 }
 
 module.exports = SellScreen;
--- styles.js
+++ styles.js
@@ -17,6 +17,59 @@ const styles = StyleSheet.create ({
     width: 26,
     height: 26,
   },
+  input: {
+    width: 250,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    margin: 5,
+    textAlign: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    margin: 10,
+  },
+  textbookCondition: {
+    width: 250,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    margin: 5,
+    alignItems: 'center',
+  },
+  textbookConditionPicker: {
+    width: 110,
+    height: 40,
+    marginLeft: 30,
+  },
+  textbookType: {
+    width: 250,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    margin: 5,
+    alignItems: 'center',
+  },
+  textbookTypePicker: {
+    width: 120,
+    height: 40,
+    marginLeft: 25,
+    alignItems: 'center',
+  },
+  submitButton: {
+    width: 70,
+    height: 40,
+    margin: 10,
+    borderRadius: 5,
+    backgroundColor: '#9932CD',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  submitButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
 
 module.exports = styles;
