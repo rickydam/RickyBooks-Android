285bfeae5b02a75a1fecac2c07a2fa03be3aa8d5
Ricky Dam <RickyDamOttawa@gmail.com>
2017-09-20 15:05:32 -0400

    Implemented KeyboardAvoidingView to prevent view obstruction

--- screens/sell.js
+++ screens/sell.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import {Text, View, Image, TextInput, Picker, TouchableOpacity, ScrollView} from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  TextInput,
+  Picker,
+  TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView
+} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
 const mainStyles = require('../styles/mainStyles.js');
@@ -43,88 +52,93 @@ class SellScreen extends React.Component {
   };
   render() {
     const {navigate} = this.props.navigation;
+    const keyboardOffset = 25;
     return (
-      <ScrollView contentContainerStyle={mainStyles.container}>
+      <KeyboardAvoidingView
+        behavior='position'
+        keyboardVerticalOffset={keyboardOffset}>
 
-        <Text style={mainStyles.title}>Sell a textbook!</Text>
+        <ScrollView contentContainerStyle={mainStyles.container}>
 
-        <TouchableOpacity onPress={() => this.uploadTextbookImage()}>
-          <View style={sellStyles.uploadButton}>
-            <Text style={sellStyles.uploadButtonText}>UPLOAD TEXTBOOK IMAGE</Text>
-          </View>
-        </TouchableOpacity>
+          <Text style={mainStyles.title}>Sell a textbook!</Text>
 
-        <View style={sellStyles.textbookImageContainer}>
-          <Image
-            source={
-              this.state.textbookImage != '' ?
-              this.state.textbookImage :
-              placeholderImg}
-            style={
-              this.state.textbookImage != '' ?
-              sellStyles.textbookImage :
-              sellStyles.textbookImageBlank} />
-        </View>
-
-        <TextInput
-          style={sellStyles.input}
-          placeholder="Title"
-          underlineColorAndroid="transparent"
-        />
-
-        <TextInput
-          style={sellStyles.input}
-          placeholder="Author"
-          underlineColorAndroid="transparent"
-        />
-
-        <TextInput
-          style={sellStyles.input}
-          placeholder="Edition"
-          underlineColorAndroid="transparent"
-        />
-
-        <View style={sellStyles.textbookCondition}>
-          <Picker
-            style={sellStyles.textbookConditionPicker}
-            itemTextStyle={sellStyles.testPickerItem}
-            selectedValue={this.state.textbookCondition}
-            onValueChange={(itemValue, itemIndex) => this.setState({textbookCondition: itemValue})}>
-            <Picker.Item label="Condition" value="" />
-            <Picker.Item label="Like New" value="Like New" />
-            <Picker.Item label="Good" value="Good" />
-            <Picker.Item label="Okay" value="Okay" />
-          </Picker>
-        </View>
-
-        <View style={sellStyles.textbookType}>
-          <Picker
-            style={sellStyles.textbookTypePicker}
-            itemStyle={sellStyles.testPickerItem}
-            selectedValue={this.state.textbookType}
-            onValueChange={(itemValue, itemIndex) => this.setState({textbookType: itemValue})}>
-            <Picker.Item label="Type" value="" />
-            <Picker.Item label="Paperback" value="Paperback" />
-            <Picker.Item label="Hardcover" value="Hardcover" />
-            <Picker.Item label="Looseleaf" value="Looseleaf" />
-          </Picker>
-        </View>
-
-        <TextInput
-          style={sellStyles.input}
-          placeholder="Course Code"
-          underlineColorAndroid="transparent"
-        />
-
-        <View style={sellStyles.submitContainer}>
-          <TouchableOpacity onPress={() => checkConditionValue(this.state.textbookCondition)}>
-            <View style={sellStyles.submitButton}>
-              <Text style={sellStyles.submitButtonText}>SUBMIT</Text>
+          <TouchableOpacity onPress={() => this.uploadTextbookImage()}>
+            <View style={sellStyles.uploadButton}>
+              <Text style={sellStyles.uploadButtonText}>UPLOAD TEXTBOOK IMAGE</Text>
             </View>
           </TouchableOpacity>
-        </View>
 
-      </ScrollView>
+          <View style={sellStyles.textbookImageContainer}>
+            <Image
+              source={
+                this.state.textbookImage != '' ?
+                this.state.textbookImage :
+                placeholderImg}
+              style={
+                this.state.textbookImage != '' ?
+                sellStyles.textbookImage :
+                sellStyles.textbookImageBlank} />
+          </View>
+
+          <TextInput
+            style={sellStyles.input}
+            placeholder="Title"
+            underlineColorAndroid="transparent"
+          />
+
+          <TextInput
+            style={sellStyles.input}
+            placeholder="Author"
+            underlineColorAndroid="transparent"
+          />
+
+          <TextInput
+            style={sellStyles.input}
+            placeholder="Edition"
+            underlineColorAndroid="transparent"
+          />
+
+          <View style={sellStyles.textbookCondition}>
+            <Picker
+              style={sellStyles.textbookConditionPicker}
+              selectedValue={this.state.textbookCondition}
+              onValueChange={(itemValue, itemIndex) => this.setState({textbookCondition: itemValue})}>
+              <Picker.Item label="Condition" value="" />
+              <Picker.Item label="Like New" value="Like New" />
+              <Picker.Item label="Good" value="Good" />
+              <Picker.Item label="Okay" value="Okay" />
+            </Picker>
+          </View>
+
+          <View style={sellStyles.textbookType}>
+            <Picker
+              style={sellStyles.textbookTypePicker}
+              itemStyle={sellStyles.testPickerItem}
+              selectedValue={this.state.textbookType}
+              onValueChange={(itemValue, itemIndex) => this.setState({textbookType: itemValue})}>
+              <Picker.Item label="Type" value="" />
+              <Picker.Item label="Paperback" value="Paperback" />
+              <Picker.Item label="Hardcover" value="Hardcover" />
+              <Picker.Item label="Looseleaf" value="Looseleaf" />
+            </Picker>
+          </View>
+
+          <TextInput
+            style={sellStyles.input}
+            placeholder="Course Code"
+            underlineColorAndroid="transparent"
+          />
+
+          <View style={sellStyles.submitContainer}>
+            <TouchableOpacity onPress={() => checkConditionValue(this.state.textbookCondition)}>
+              <View style={sellStyles.submitButton}>
+                <Text style={sellStyles.submitButtonText}>SUBMIT</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }
