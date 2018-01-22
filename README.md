78d50db50ff4294e39c95d692278253ed17069bd
Ricky Dam <RickyDamOttawa@gmail.com>
2018-01-22 01:36:41 -0500

    Use Android Studio instead of React-Native. Set up navigation.

--- README.md
+++
@@ -1,2 +0,0 @@
-# RickyBooks
-A mobile app for University students to buy or list&sell textbooks and message each other to arrange a meetup time and place.
---
+++ app/src/main/java/com/example/rickydam/rickybooks/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.rickydam.rickybooks;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_buy:
+                    mTextMessage.setText(R.string.title_buy);
+                    return true;
+                case R.id.navigation_messages:
+                    mTextMessage.setText(R.string.title_messages);
+                    return true;
+                case R.id.navigation_sell:
+                    mTextMessage.setText(R.string.title_sell);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
