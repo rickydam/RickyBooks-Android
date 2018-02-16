1893f877b8a9656b0cbc6ff88ce8ecc123298cb5
Ricky Dam <RickyDamOttawa@gmail.com>
2018-02-16 00:46:45 -0500

    Improve fragment handling

--- app/src/main/java/com/example/rickydam/rickybooks/MainActivity.java
+++ app/src/main/java/com/example/rickydam/rickybooks/MainActivity.java
@@ -3,55 +3,88 @@ package com.example.rickydam.rickybooks;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    HomeFragment homeFragment;
+    BuyFragment buyFragment;
+    MessagesFragment messagesFragment;
+    SellFragment sellFragment;
+    ProfileFragment profileFragment;
+    Fragment currentFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
+
                 case R.id.navigation_home:
                     setTitle("RickyBooks");
-                    HomeFragment homeFragment = new HomeFragment();
-                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction1.replace(R.id.frame, homeFragment);
-                    fragmentTransaction1.commit();
+                    if(currentFragment != homeFragment) {
+                        FragmentTransaction fragmentTransaction;
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(currentFragment);
+                        fragmentTransaction.show(homeFragment);
+                        fragmentTransaction.commit();
+                        currentFragment = homeFragment;
+                    }
                     return true;
+
                 case R.id.navigation_buy:
                     setTitle("Buy a textbook!");
-                    BuyFragment buyFragment = new BuyFragment();
-                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction2.replace(R.id.frame, buyFragment);
-                    fragmentTransaction2.commit();
+                    if(currentFragment != buyFragment) {
+                        FragmentTransaction fragmentTransaction;
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(currentFragment);
+                        fragmentTransaction.show(buyFragment);
+                        fragmentTransaction.commit();
+                        currentFragment = buyFragment;
+                    }
                     return true;
+
                 case R.id.navigation_messages:
                     setTitle("Messages");
-                    MessagesFragment messagesFragment = new MessagesFragment();
-                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction3.replace(R.id.frame, messagesFragment);
-                    fragmentTransaction3.commit();
+                    if(currentFragment != messagesFragment) {
+                        FragmentTransaction fragmentTransaction;
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(currentFragment);
+                        fragmentTransaction.show(messagesFragment);
+                        fragmentTransaction.commit();
+                        currentFragment = messagesFragment;
+                    }
                     return true;
+
                 case R.id.navigation_sell:
                     setTitle("Sell a textbook!");
-                    SellFragment sellFragment = new SellFragment();
-                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction4.replace(R.id.frame, sellFragment);
-                    fragmentTransaction4.commit();
+                    if(currentFragment != sellFragment) {
+                        FragmentTransaction fragmentTransaction;
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(currentFragment);
+                        fragmentTransaction.show(sellFragment);
+                        fragmentTransaction.commit();
+                        currentFragment = sellFragment;
+                    }
                     return true;
+
                 case R.id.navigation_profile:
                     setTitle("Profile");
-                    ProfileFragment profileFragment = new ProfileFragment();
-                    FragmentTransaction fragmentTransaction5 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction5.replace(R.id.frame, profileFragment);
-                    fragmentTransaction5.commit();
+                    if(currentFragment != profileFragment) {
+                        FragmentTransaction fragmentTransaction;
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(currentFragment);
+                        fragmentTransaction.show(profileFragment);
+                        fragmentTransaction.commit();
+                        currentFragment = profileFragment;
+                    }
                     return true;
+
+                default:
+                    return false;
             }
-            return false;
         }
     };
 
@@ -60,15 +93,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         BottomNavigationViewHelper.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        setTitle("RickyBooks");
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentTransaction fragmentTransaction0 = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction0.replace(R.id.frame, homeFragment);
-        fragmentTransaction0.commit();
-    }
+        homeFragment = new HomeFragment();
+        buyFragment = new BuyFragment();
+        messagesFragment = new MessagesFragment();
+        sellFragment = new SellFragment();
+        profileFragment = new ProfileFragment();
 
+        FragmentTransaction fragmentTransaction;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container, homeFragment);
+        currentFragment = homeFragment;
+
+        fragmentTransaction.add(R.id.fragment_container, buyFragment);
+        fragmentTransaction.hide(buyFragment);
+
+        fragmentTransaction.add(R.id.fragment_container, messagesFragment);
+        fragmentTransaction.hide(messagesFragment);
+
+        fragmentTransaction.add(R.id.fragment_container, sellFragment);
+        fragmentTransaction.hide(sellFragment);
+
+        fragmentTransaction.add(R.id.fragment_container, profileFragment);
+        fragmentTransaction.hide(profileFragment);
+
+        fragmentTransaction.commit();
+    }
 }
