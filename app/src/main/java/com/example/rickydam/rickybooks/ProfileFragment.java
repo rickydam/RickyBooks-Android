add397e1cad511eaf5d588aa55808444577a6ef2
Ricky Dam <RickyDamOttawa@gmail.com>
2018-03-11 23:49:41 -0400

    Add LogOut button to ProfileFragment

--- app/src/main/java/com/example/rickydam/rickybooks/ProfileFragment.java
+++ app/src/main/java/com/example/rickydam/rickybooks/ProfileFragment.java
@@ -1,23 +1,70 @@
 package com.example.rickydam.rickybooks;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileFragment extends Fragment {
-
     public ProfileFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button button = view.findViewById(R.id.logout_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Context context= getActivity().getApplicationContext();
+                RequestQueue queue = Volley.newRequestQueue(context);
+                String url = "http://192.168.0.110:3000/logout";
+                StringRequest stringRequest = new StringRequest(Request.Method.DELETE, url,
+                        new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ((MainActivity) getActivity()).logout();
+                        ((MainActivity) getActivity()).replaceFragment("ProfileFragment");
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Ricky", String.valueOf(error));
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<>();
+                        String token = ((MainActivity) getActivity()).getToken();
+                        headers.put("Authorization", "Token token=" + token);
+                        return headers;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+
+        return view;
     }
 
 }
