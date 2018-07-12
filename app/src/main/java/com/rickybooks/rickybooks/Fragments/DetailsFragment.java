378502cbaeeaf9aab85a522ce87ddb0064e0401b
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-11 20:21:27 -0400

    Hide the message button if the textbook belongs to that user

--- app/src/main/java/com/rickybooks/rickybooks/Fragments/DetailsFragment.java
+++ app/src/main/java/com/rickybooks/rickybooks/Fragments/DetailsFragment.java
@@ -97,6 +97,18 @@ public class DetailsFragment extends Fragment {
         else {
             Glide.with(this).load(R.drawable.textbook_placeholder).into(detailsImage);
         }
+
+        MainActivity activity = (MainActivity) getActivity();
+        String textbookSellerId = bundle.getString("SellerId");
+        if(activity.getUserId() != null) {
+            // User is logged in
+            String appUserId = activity.getUserId();
+            if(appUserId.equals(textbookSellerId)) {
+                // Textbook belongs to this user, hide the MESSAGE button
+                Button messageButton = view.findViewById(R.id.details_message_button);
+                messageButton.setVisibility(View.GONE);
+            }
+        }
     }
 
     public void messageButtonPressed() {
@@ -111,58 +123,49 @@ public class DetailsFragment extends Fragment {
     }
 
     public void createSendDialog() {
+        MainActivity activity = (MainActivity) getActivity();
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(activity,
+                R.style.AppTheme_AlertDialogTheme);
+
+        final EditText messageInput = new EditText(activity);
+        messageInput.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        messageInput.setSingleLine(false);
+        messageInput.setHint("Type a message...");
+        messageInput.setBackgroundResource(R.drawable.border);
+        messageInput.setPadding(20, 15, 20, 15);
+        messageInput.setHintTextColor(getResources().getColor(R.color.lightGray));
+        messageInput.setMinWidth(1000);
+        messageInput.setMaxHeight(500);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(60, 10, 60, 0);
+        messageInput.setLayoutParams(layoutParams);
+
+        LinearLayout parentLayout = new LinearLayout(activity);
+        parentLayout.addView(messageInput);
+
         Bundle bundle = getArguments();
         String sellerName = (String) bundle.get("SellerName");
-        Context context = getActivity();
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                "com.rickybooks.rickybooks", Context.MODE_PRIVATE);
-        String currentPersonName = sharedPref.getString("name", null);
+        alertDialog.setTitle("Message " + sellerName);
+        alertDialog.setView(parentLayout);
+        alertDialog.setCancelable(false);
 
-        if(!sellerName.equals(currentPersonName)) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(context,
-                    R.style.AppTheme_AlertDialogTheme);
-
-            final EditText messageInput = new EditText(context);
-            messageInput.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
-            messageInput.setSingleLine(false);
-            messageInput.setHint("Type a message...");
-            messageInput.setBackgroundResource(R.drawable.border);
-            messageInput.setPadding(20, 15, 20, 15);
-            messageInput.setHintTextColor(getResources().getColor(R.color.lightGray));
-            messageInput.setMinWidth(1000);
-            messageInput.setMaxHeight(500);
-
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(60, 10, 60, 0);
-            messageInput.setLayoutParams(layoutParams);
-
-            LinearLayout parentLayout = new LinearLayout(context);
-            parentLayout.addView(messageInput);
-
-            alertDialog.setTitle("Message " + sellerName);
-            alertDialog.setView(parentLayout);
-            alertDialog.setCancelable(false);
-
-            alertDialog.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    String message = String.valueOf(messageInput.getText());
-                    createConversationReq(message);
-                    dialog.dismiss();
-                }
-            });
-            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-            alertDialog.show();
-        }
-        else {
-            createAlert("This is your textbook!", "Don't send yourself messages...");
-        }
+        alertDialog.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String message = String.valueOf(messageInput.getText());
+                createConversationReq(message);
+                dialog.dismiss();
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 
     public void createConversationReq(final String message) {
