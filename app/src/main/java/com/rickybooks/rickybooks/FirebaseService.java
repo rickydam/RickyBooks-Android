5af19b3941e900abc327db174c1b77f834067276
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-16 18:14:39 -0400

    Create a Firebase service to handle notifications

---
+++ app/src/main/java/com/rickybooks/rickybooks/FirebaseService.java
@@ -0,0 +1,65 @@
+package com.rickybooks.rickybooks;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage.getNotification() != null) {
+            String title = remoteMessage.getNotification().getTitle();
+            String message = remoteMessage.getNotification().getBody();
+            doNotification(title, message);
+        }
+    }
+
+    public void doNotification(String title, String message) {
+        createNotificationChannel();
+
+        Context context = getApplicationContext();
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Ricky");
+        builder.setSmallIcon(R.drawable.ic_book);
+        builder.setContentTitle(title);
+        builder.setContentText(message);
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pendingIntent);
+        builder.build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(12345, builder.build());
+    }
+
+    public void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because the
+        // NotificationChannel class is new and not in the support library
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "RickyChannel";
+            String description = "RickyDescription";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("Ricky", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            // You can't change the importance or other notification behaviours after this
+            Context context = getApplicationContext();
+            NotificationManager notificationManager = context.getSystemService(
+                    NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
