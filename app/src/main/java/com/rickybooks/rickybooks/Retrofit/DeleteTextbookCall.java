2c08d3a303941ba2c93d423e2a9bbbd18433649d
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-21 22:23:33 -0400

    Create a class to handle deleteTextbook

---
+++ app/src/main/java/com/rickybooks/rickybooks/Retrofit/DeleteTextbookCall.java
@@ -0,0 +1,47 @@
+package com.rickybooks.rickybooks.Retrofit;
+
+import com.rickybooks.rickybooks.MainActivity;
+import com.rickybooks.rickybooks.Other.Alert;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class DeleteTextbookCall {
+    private MainActivity activity;
+    private TextbookService textbookService;
+    private String signedDeleteUrl;
+
+    public DeleteTextbookCall(MainActivity activity) {
+        this.activity = activity;
+        Retrofit retrofit = new RetrofitClient().getClient();
+        textbookService = retrofit.create(TextbookService.class);
+    }
+
+    public String getData() {
+        return signedDeleteUrl;
+    }
+
+    public void req(String textbookId) {
+        String token = activity.getToken();
+        String tokenString = "Token token=" + token;
+
+        try {
+            Call<String> call = textbookService.deleteTextbook(tokenString, textbookId);
+            Response<String> response = call.execute();
+
+            if(response.isSuccessful()) {
+                signedDeleteUrl = response.body();
+            }
+            else {
+                Alert alert = new Alert(activity);
+                alert.create("Oh no! Server problem!", "Seems like we are unable to " +
+                        "reach the server at the moment.\n\nPlease try again later.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
