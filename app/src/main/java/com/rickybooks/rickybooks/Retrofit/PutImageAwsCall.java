5526a755ecc6c984790d9b26dc05d46c54feb17c
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-24 02:01:12 -0400

    Create a class to handle putImageAws

---
+++ app/src/main/java/com/rickybooks/rickybooks/Retrofit/PutImageAwsCall.java
@@ -0,0 +1,45 @@
+package com.rickybooks.rickybooks.Retrofit;
+
+import com.rickybooks.rickybooks.MainActivity;
+import com.rickybooks.rickybooks.Other.Alert;
+
+import java.io.IOException;
+
+import okhttp3.RequestBody;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class PutImageAwsCall {
+    private MainActivity activity;
+    private boolean isSuccessful;
+
+    public PutImageAwsCall(MainActivity activity) {
+        this.activity = activity;
+        isSuccessful = false;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public void req(String signedPostUrl, RequestBody reqFile) {
+        Retrofit retrofit = new RetrofitClient().getClient();
+        TextbookService textbookService = retrofit.create(TextbookService.class);
+
+        try {
+            Call<Void> call = textbookService.putImageAws(signedPostUrl, reqFile);
+            Response<Void> response = call.execute();
+
+            isSuccessful = response.isSuccessful();
+            if(!isSuccessful) {
+                Alert alert = new Alert(activity);
+                alert.create("Oh no! Server problem!", "Seems like we are unable to " +
+                        "reach the server at the moment.\n\nPlease try again later.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
