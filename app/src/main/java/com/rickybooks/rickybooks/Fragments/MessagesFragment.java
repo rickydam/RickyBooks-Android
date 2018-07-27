f09ddbf0c0446619ef3e8e2b3caf4c42f69086b7
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-26 22:26:13 -0400

    Modularize the Messages Fragment

--- app/src/main/java/com/rickybooks/rickybooks/Fragments/MessagesFragment.java
+++ app/src/main/java/com/rickybooks/rickybooks/Fragments/MessagesFragment.java
@@ -1,9 +1,6 @@
 package com.rickybooks.rickybooks.Fragments;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,45 +15,30 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
 import com.rickybooks.rickybooks.Adapters.MessageAdapter;
 import com.rickybooks.rickybooks.MainActivity;
 import com.rickybooks.rickybooks.Models.Message;
 import com.rickybooks.rickybooks.R;
-import com.rickybooks.rickybooks.Retrofit.RetrofitClient;
-import com.rickybooks.rickybooks.Retrofit.TextbookService;
+import com.rickybooks.rickybooks.Retrofit.GetMessagesCall;
+import com.rickybooks.rickybooks.Retrofit.SendMessageCall;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class MessagesFragment extends Fragment {
-    private List<Message> messagesList = new ArrayList<>();
+    private List<Message> messages = new ArrayList<>();
     private MessageAdapter messageAdapter;
-    private TextbookService textbookService;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Retrofit retrofit = new RetrofitClient().getClient();
-        textbookService = retrofit.create(TextbookService.class);
-        getMessagesReq();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getMessages();
+            }
+        }).start();
     }
 
     @Override
@@ -69,7 +50,12 @@ public class MessagesFragment extends Fragment {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                getMessagesReq();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getMessages();
+                    }
+                }).start();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
@@ -78,14 +64,19 @@ public class MessagesFragment extends Fragment {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                sendMessageReq(view);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendMessage(view);
+                    }
+                }).start();
             }
         });
 
         MainActivity activity = (MainActivity) getActivity();
         LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
         layoutManager.setStackFromEnd(true);
-        messageAdapter = new MessageAdapter(activity, messagesList);
+        messageAdapter = new MessageAdapter(activity, messages);
 
         RecyclerView messageRecycler = view.findViewById(R.id.messages_recycler);
         messageRecycler.setHasFixedSize(true);
@@ -95,133 +86,45 @@ public class MessagesFragment extends Fragment {
         return view;
     }
 
-    public void getMessagesReq() {
-        clearMessages();
+    public void getMessages() {
+        messages.clear();
         MainActivity activity = (MainActivity) getActivity();
-        String tokenString = getTokenString();
-        String conversationId = activity.getConversationId();
+        Bundle bundle = getArguments();
+        String conversationId = bundle.getString("conversation_id");
 
-        Call<JsonArray> call = textbookService.getMessages(tokenString, conversationId);
-        call.enqueue(new Callback<JsonArray>() {
+        GetMessagesCall getMessagesCall = new GetMessagesCall(activity);
+        getMessagesCall.req(conversationId);
+        messages.addAll(getMessagesCall.getData());
+
+        activity.runOnUiThread(new Runnable() {
             @Override
-            public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
-                getMessagesRes(response);
-            }
-            @Override
-            public void onFailure(Call<JsonArray> call, Throwable t) {
-                Log.e("Ricky", "getMessagesReq failure: " + t.getMessage());
-                createAlert("Oh no! Server problem!", "Seems like we are unable to " +
-                        "reach the server at the moment.\n\nPlease try again later.");
+            public void run() {
+                messageAdapter.notifyDataSetChanged();
             }
         });
     }
 
-    public void getMessagesRes(Response<JsonArray> response) {
-        if(response.isSuccessful()) {
-            try {
-                String res = String.valueOf(response.body());
-                JSONArray resData = new JSONArray(res);
-                for(int i=0; i<resData.length(); i++) {
-                    JSONObject obj = resData.getJSONObject(i);
+    public void sendMessage(final View view) {
+        MainActivity activity = (MainActivity) getActivity();
+        Bundle bundle = getArguments();
+        String conversationId = bundle.getString("conversation_id");
+        final EditText messageInput = view.findViewById(R.id.messages_message_input);
+        String body = String.valueOf(messageInput.getText());
 
-                    String body = obj.getString("body");
-                    JSONObject user = obj.getJSONObject("user");
-                    String sender = user.getString("name");
+        SendMessageCall sendMessageCall = new SendMessageCall(activity);
+        sendMessageCall.req(conversationId, body);
 
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-                            Locale.CANADA);
-                    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-                    String created_at = obj.getString("created_at");
-                    Date date = sdf.parse(created_at);
-
-                    Message message = new Message(body, sender, date);
-                    messagesList.add(message);
-                    messageAdapter.notifyDataSetChanged();
+        if(sendMessageCall.isSuccessful()) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageInput.getText().clear();
+                    messageInput.clearFocus();
+                    hideKeyboard(view);
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            });
+            getMessages();
         }
-        else {
-            try {
-                String errorMessage = response.errorBody().string();
-                Log.e("Ricky", "getMessagesReq unsuccessful: " + errorMessage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            createAlert("Oh no! Server problem!", "Seems like we are unable to " +
-                    "reach the server at the moment.\n\nPlease try again later.");
-        }
-    }
-
-    public void sendMessageReq(final View view) {
-        String tokenString = getTokenString();
-        MainActivity activity = (MainActivity) getActivity();
-        String conversationId = activity.getConversationId();
-        EditText messageInput = view.findViewById(R.id.messages_message_input);
-        String message = String.valueOf(messageInput.getText());
-        String userId = activity.getUserId();
-
-        Call<JsonObject> call = textbookService.sendMessage(tokenString, conversationId, message,
-                userId);
-        call.enqueue(new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                sendMessageRes(view, response);
-            }
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-                Log.e("Ricky", "sendMessageReq failure: " + t.getMessage());
-                createAlert("Oh no! Server problem!", "Seems like we are unable to " +
-                        "reach the server at the moment.\n\nPlease try again later.");
-            }
-        });
-    }
-
-    public void sendMessageRes(View v, Response<JsonObject> response) {
-        if(response.isSuccessful()) {
-            EditText messageInput = v.findViewById(R.id.messages_message_input);
-            messageInput.getText().clear();
-            hideKeyboard(v);
-            getMessagesReq();
-        }
-        else {
-            try {
-                String errorMessage = response.errorBody().string();
-                Log.e("Ricky", "sendMessageReq unsuccessful: " + errorMessage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            createAlert("Seems like you forgot something...", "That's right! " +
-                    "The message!");
-        }
-    }
-
-    public void clearMessages() {
-        messagesList.clear();
-    }
-
-    public String getTokenString() {
-        MainActivity activity = (MainActivity) getActivity();
-        String token = activity.getToken();
-        return "Token token=" + token;
-    }
-
-    public void createAlert(String title, String message) {
-        Activity activity = getActivity();
-        AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
-        alertDialog.setTitle(title);
-        alertDialog.setMessage(message);
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
-                new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        alertDialog.show();
     }
 
     public void hideKeyboard(View view) {
