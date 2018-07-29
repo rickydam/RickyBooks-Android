baf05590a3b9c7fbaab16c5980c9dd211bd448e4
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-29 17:32:43 -0400

    Create a class to handle deleteImageEntry

---
+++ app/src/main/java/com/rickybooks/rickybooks/Retrofit/DeleteImageEntryCall.java
@@ -0,0 +1,46 @@
+package com.rickybooks.rickybooks.Retrofit;
+
+import com.rickybooks.rickybooks.MainActivity;
+import com.rickybooks.rickybooks.Other.Alert;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class DeleteImageEntryCall {
+    private MainActivity activity;
+    private boolean isSuccessful;
+
+    public DeleteImageEntryCall(MainActivity activity) {
+        this.activity = activity;
+        isSuccessful = false;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public void req(String textbookId) {
+        String token = activity.getToken();
+        String tokenString = "Token token=" + token;
+
+        try {
+            Retrofit retrofit = new RetrofitClient().getClient();
+            TextbookService textbookService = retrofit.create(TextbookService.class);
+
+            Call<Void> call = textbookService.deleteImageEntry(tokenString, textbookId);
+            Response<Void> response = call.execute();
+
+            isSuccessful = response.isSuccessful();
+            if(!isSuccessful) {
+                Alert alert = new Alert(activity);
+                alert.create("Oh no! Server problem!", "Seems like we are unable to " +
+                        "reach the server at the moment.\n\nPlease try again later.");
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
