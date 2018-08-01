275c1a1c3a7d6238e5a9d92c6549fb9522784bc9
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-01 04:39:17 -0400

    Integrate the menu with search option into the BuyFragment

--- app/src/main/java/com/rickybooks/rickybooks/Fragments/BuyFragment.java
+++ app/src/main/java/com/rickybooks/rickybooks/Fragments/BuyFragment.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -28,6 +31,7 @@ public class BuyFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         textbooks = new ArrayList<>();
 
         new Thread(new Runnable() {
@@ -69,6 +73,22 @@ public class BuyFragment extends Fragment {
                 DividerItemDecoration.VERTICAL));
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.buy, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.buy_menu_search:
+                MainActivity activity = (MainActivity) getActivity();
+                activity.replaceFragment("SearchFragment");
+        }
+        return true;
     }
 
     public void getTextbooks() {
