db212d5aca8f553aaa0043d35f5f30ce41a24753
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-22 15:14:14 -0400

    Remove unused imports from the ConversationsAdapter

--- app/src/main/java/com/rickybooks/rickybooks/Adapters/ConversationAdapter.java
+++ app/src/main/java/com/rickybooks/rickybooks/Adapters/ConversationAdapter.java
@@ -1,7 +1,5 @@
 package com.rickybooks.rickybooks.Adapters;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
