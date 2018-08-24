0be5dd93a9294a5584aff8353c96ebb5eac67f43
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-24 17:53:49 -0400

    Edit the GetConversations class to handle the conversation last messages

--- app/src/main/java/com/rickybooks/rickybooks/Retrofit/GetConversationsCall.java
+++ app/src/main/java/com/rickybooks/rickybooks/Retrofit/GetConversationsCall.java
@@ -1,9 +1,10 @@
 package com.rickybooks.rickybooks.Retrofit;
 
-import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import com.rickybooks.rickybooks.MainActivity;
 import com.rickybooks.rickybooks.Models.Conversation;
+import com.rickybooks.rickybooks.Models.Message;
 import com.rickybooks.rickybooks.Other.Alert;
 
 import org.json.JSONArray;
@@ -11,8 +12,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -21,14 +27,20 @@ import retrofit2.Retrofit;
 public class GetConversationsCall {
     private MainActivity activity;
     private List<Conversation> conversations;
+    private List<Message> lastMessages;
 
     public GetConversationsCall(MainActivity activity) {
         this.activity = activity;
         conversations = new ArrayList<>();
+        lastMessages = new ArrayList<>();
     }
 
-    public List<Conversation> getData() {
+    public List<Conversation> getConversationsData() {
         return conversations;
+    }
+
+    public List<Message> getLastMessagesData() {
+        return lastMessages;
     }
 
     public void req() {
@@ -40,11 +52,21 @@ public class GetConversationsCall {
             Retrofit retrofit = new RetrofitClient().getClient();
             TextbookService textbookService = retrofit.create(TextbookService.class);
 
-            Call<JsonArray> call = textbookService.getConversations(tokenString, userId);
-            Response<JsonArray> response = call.execute();
+            Call<JsonObject> call = textbookService.getConversations(tokenString, userId);
+            Response<JsonObject> response = call.execute();
 
             if(response.isSuccessful()) {
-                parseConversationData(response.body());
+                JsonObject responseObj = response.body();
+                String conversationStr = String.valueOf(responseObj.get("conversations"));
+                String lastMessagesStr = String.valueOf(responseObj.get("last_messages"));
+                try {
+                    JSONArray conversationData = new JSONArray(conversationStr);
+                    parseConversationData(conversationData);
+                    JSONArray lastMessagesData =  new JSONArray(lastMessagesStr);
+                    parseMessageData(lastMessagesData);
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
             }
             else {
                 Alert alert = new Alert(activity);
@@ -56,13 +78,10 @@ public class GetConversationsCall {
         }
     }
 
-    private void parseConversationData(JsonArray responseArr) {
+    private void parseConversationData(JSONArray conversationsData) {
         try {
-            String res = String.valueOf(responseArr);
-            JSONArray resData = new JSONArray(res);
-
-            for(int i=0; i<resData.length(); i++) {
-                JSONObject obj = resData.getJSONObject(i);
+            for(int i=0; i<conversationsData.length(); i++) {
+                JSONObject obj = conversationsData.getJSONObject(i);
                 String conversationId = obj.getString("id");
 
                 JSONObject recipientObj = obj.getJSONObject("recipient");
@@ -83,6 +102,31 @@ public class GetConversationsCall {
                 conversations.add(conversation);
             }
         } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseMessageData(JSONArray lastMessagesData) {
+        try {
+            for(int i=0; i<lastMessagesData.length(); i++) {
+                JSONObject obj = lastMessagesData.getJSONObject(i);
+
+                String body = obj.getString("body");
+                JSONObject user = obj.getJSONObject("user");
+                String sender = user.getString("name");
+
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+                        Locale.CANADA);
+                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                String created_at = obj.getString("created_at");
+                Date date = sdf.parse(created_at);
+
+                Message message = new Message(body, sender, date);
+                lastMessages.add(message);
+            }
+        } catch(JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
