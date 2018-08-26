e11b4bd4e7524e75d3617593e04fd80bc044f7c0
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-26 14:07:53 -0400

    Add onClick and onLongClick to the NotifyItemAdapter

--- app/src/main/java/com/rickybooks/rickybooks/Adapters/NotifyItemAdapter.java
+++ app/src/main/java/com/rickybooks/rickybooks/Adapters/NotifyItemAdapter.java
@@ -1,18 +1,22 @@
 package com.rickybooks.rickybooks.Adapters;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.rickybooks.rickybooks.Fragments.NotifyFragment;
+import com.rickybooks.rickybooks.MainActivity;
 import com.rickybooks.rickybooks.Models.NotifyItem;
 import com.rickybooks.rickybooks.R;
 
 import java.util.List;
 
 public class NotifyItemAdapter extends RecyclerView.Adapter<NotifyItemAdapter.NotifyItemViewHolder> {
+    private MainActivity activity;
     private List<NotifyItem> notifyItems;
 
     class NotifyItemViewHolder extends RecyclerView.ViewHolder {
@@ -26,7 +30,8 @@ public class NotifyItemAdapter extends RecyclerView.Adapter<NotifyItemAdapter.No
         }
     }
 
-    public NotifyItemAdapter(List<NotifyItem> notifyItems) {
+    public NotifyItemAdapter(MainActivity activity, List<NotifyItem> notifyItems) {
+        this.activity = activity;
         this.notifyItems = notifyItems;
     }
 
@@ -38,10 +43,62 @@ public class NotifyItemAdapter extends RecyclerView.Adapter<NotifyItemAdapter.No
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NotifyItemViewHolder holder, int position) {
-        NotifyItem notifyItem = notifyItems.get(holder.getAdapterPosition());
+    public void onBindViewHolder(@NonNull final NotifyItemViewHolder holder, int position) {
+        final NotifyItem notifyItem = notifyItems.get(holder.getAdapterPosition());
         holder.notifyItemCategory.setText(notifyItem.getCategory());
         holder.notifyItemInput.setText(notifyItem.getInput());
+
+        holder.itemView.setBackgroundResource(R.color.white);
+
+        final NotifyFragment notifyFragment = getNotifyFragment();
+        if(notifyFragment != null) {
+            boolean actionMode = activity.getActionMode();
+            if(actionMode) {
+                boolean notifyItemExists = notifyFragment.notifyItemExists(notifyItem);
+                if(notifyItemExists) {
+                    holder.itemView.setBackgroundResource(R.color.lightGray);
+                }
+            }
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(notifyFragment != null) {
+                    boolean actionMode = activity.getActionMode();
+                    if(actionMode) {
+                        notifyFragment.selectNotifyItem(notifyItem);
+                        notifyItemChanged(holder.getAdapterPosition());
+                    }
+                }
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if(notifyFragment != null) {
+                    boolean actionMode = activity.getActionMode();
+                    if(!actionMode) {
+                        notifyFragment.prepareSelection(notifyItem);
+                        notifyItemChanged(holder.getAdapterPosition());
+                    }
+                    else {
+                        notifyFragment.selectNotifyItem(notifyItem);
+                        notifyItemChanged(holder.getAdapterPosition());
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private NotifyFragment getNotifyFragment() {
+        FragmentManager fm = activity.getSupportFragmentManager();
+        if(fm.findFragmentByTag("NotifyFragment") != null) {
+            return (NotifyFragment) fm.findFragmentByTag("NotifyFragment");
+        }
+        return null;
     }
 
     @Override
